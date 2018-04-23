4799ad55a11e7a0779c57d43329884b8c7d374a6
RomanLili <38641078+RomanLili@users.noreply.github.com>
2018-04-22 23:16:16 -0500

    Update BIBLIOTECA.java
    corregi el error en la linea 5

--- src/biblioteca/BIBLIOTECA.java
+++ src/biblioteca/BIBLIOTECA.java
@@ -2,7 +2,7 @@ package biblioteca;
 public class BIBLIOTECA {
     public static void main(String[] args) {
         System.out.print("hola mundo");
-        System.out.print("hola mundo como estan ;)"):
+        System.out.print("hola mundo como estan ;)");
     }
     
 }
