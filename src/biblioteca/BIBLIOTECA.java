1b0123e369587eb4997b777bc7c11162846c538f
RomanLili <38641078+RomanLili@users.noreply.github.com>
2018-04-22 23:14:52 -0500

    Update BIBLIOTECA.java
    le agrege una salida de texto

--- src/biblioteca/BIBLIOTECA.java
+++ src/biblioteca/BIBLIOTECA.java
@@ -2,6 +2,7 @@ package biblioteca;
 public class BIBLIOTECA {
     public static void main(String[] args) {
         System.out.print("hola mundo");
+        System.out.print("hola mundo como estan ;)"):
     }
     
 }
