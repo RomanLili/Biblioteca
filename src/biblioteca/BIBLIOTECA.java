f2d6ed332e1632683122f0578bf274e2aae78e97
RomanLili <38641078+RomanLili@users.noreply.github.com>
2018-04-25 13:26:04 -0500

    Update BIBLIOTECA.java
    se hacen pruebas

--- src/biblioteca/BIBLIOTECA.java
+++ src/biblioteca/BIBLIOTECA.java
@@ -3,6 +3,7 @@ public class BIBLIOTECA {
     public static void main(String[] args) {
         System.out.print("hola mundo");
         System.out.print("hola mundo como estan ;)");
+        System.out.print("esto es una prueba");
     }
     
 }
